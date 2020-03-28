5fc0fb4cfa214b2bd2dd3de0940d94e401f24598
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-28 18:24:27 -0300

    Backend dependencies basics mounted with 'nodemon index.js' running when whe send npm start

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,41 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+/**
+ * Rotas / Recursos
+ */
+
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * _________________________________________________________________________________
+ * Query Params: 
+ *   Parâmetros nomeados enviados na rota após "?", servem pra filtros, exemplo:
+ *   "/users?id=1&?name=Fabielson"
+ * _________________________________________________________________________________
+ * Route Params: 
+ *   Parâmetros utilizados para identificar recursos únicos, exemplo:  
+ *   "/users/:id".
+ *  --------------------------------------------------------------------------------
+ * Request Body: 
+ *   Aqui usamos o request.body pra pegar algo num POST via "request.body"
+ * _________________________________________________________________________________
+ */
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json({
         evento: "Semana OmniStack 11.0",
         aluno: "Guilherme Borges"
