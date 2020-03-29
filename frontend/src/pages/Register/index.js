7e0c6aa78678df07cba1614af2c5e9fb17a8c114
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-29 17:58:47 -0300

    Little text bug fix

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -14,7 +14,7 @@ export default function Register() {
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     <Link className="back-link" to="/">
                         <img src={ReturnIcon} ig="LogInIcon" alt="Login Icon" />
-                        Não tenho cadastro
+                        Voltar para o logon
                     </Link>
                 </section>
                 <form>
