10300a8d35cd410ce1d0956ecdf5e55e71107710
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2020-03-29 21:36:56 -0300

    Frontend and backend connected

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -11,6 +11,7 @@ export default function Profile() {
     const [incidents, setIncidents] = useState([]);
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+    const history = useHistory();
 
     useEffect(() => {
         api.get('profile', {
@@ -35,6 +36,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -43,7 +49,7 @@ export default function Profile() {
                 <Link className="button" to="/incidents/new">
                     Cadastrar novo caso
                 </Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <img src={PowerIcon} ig="PowerIcon" alt="Power Icon" />
                 </button>
             </header>
